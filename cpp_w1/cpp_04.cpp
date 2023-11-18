9d18aab593c8d84616f0adb3a50d711dfa359a11
way-daeho <lavr2755@gmail.com>
2023-11-19 04:14:03 +0900

    Remain the Some Part.5

--- cpp_w1/cpp_04.cpp
+++ cpp_w1/cpp_04.cpp
@@ -1,5 +1,5 @@
 /*
-// new와 delete 의 사용 
+// new와 delete 의 사용
 #include <iostream>
 
 int main(){
@@ -113,7 +113,7 @@ int main(){
 // 		if (play_with < animal_num) play(list[play_with]);
 
 // 		break;
-	
+
 // 	case 3:
 // 		std::cout << "누구껄 보게? : ";
 // 		std::cin >> play_with;
@@ -128,7 +128,7 @@ int main(){
 // 		delete list[i];
 // }
 
-/* 클래스 == 객체의 '설계도' 
+/* 클래스 == 객체의 '설계도'
 	클래스를 이용해 만들어진 객체 == 인스턴스 */
 
 // #include <iostream>
@@ -162,7 +162,7 @@ int main(){
 // 	return 0;
 // }
 
-/* 생각 해볼 문제 
+/* 생각 해볼 문제
 	Date 클래스를 디자인
 	SetDate -> Date 함수 내부 초기화
 	AddDay, AddMonth, Addyear -> 일, 월, 년 원하는 만큼 더함.
@@ -171,7 +171,7 @@ int main(){
 /* Edge Point
 	1. Luna year 윤년 year % 4 == 0 일 때 2월은 29일까지
 		역은 2월 28일까지 존재.
-	2. 12월 이후로 달을 더하게 된다면, 
+	2. 12월 이후로 달을 더하게 된다면,
 		1. year가 1 증가해야함.
 		2. 달 별로 max_day가 다르기 때문에 이 부분도 생각해야함
 			1,3,5,7,8,10,12 -> 31
@@ -200,7 +200,7 @@ class Date {
 			}
 			else
 				year_ = year;
-			
+
 			// month와 date 초기화 부분 -> date는 month의 값의 종속이 된다.
 			if (month <= 0 || 12 < month || date < 1 || 31 < date)
 			{
@@ -255,7 +255,7 @@ class Date {
 			if ((year_ % 4 == 0 && year_ % 100 != 0 && month_ ==2) || (year_ % 400 == 0 && month_ == 2))
 				MD = 29;
 			if (day_ + inc <= MD)
-			{ 
+			{
 				day_ += inc; // 해당 달에 포함되는 경우
 				return ;
 			}
@@ -307,7 +307,7 @@ class Date {
 		{
 			year_ += inc;
 		};
-	
+
 	void showDate()
 	{
 		std::cout << year_ << " 년 " << month_ << " 월 " << day_ << " 일 " << std::endl;
@@ -326,3 +326,181 @@ int main()
 
 예외처리를 하긴 했지만, 윤년을 구하는 함수를 만들어서 사용하는 것이 더 좋을 것 같다. 주어진 조건만 사용하긴 했는데.
 */
+
+/* 함수의 오버로딩 */
+// #include <iostream>
+
+// void print(int x) { std::cout << "int : " << x <<std::endl; }
+// void print(double x) { std::cout << "double : " << x <<std::endl; }
+
+// int main() {
+// 	int a = 1;
+// 	char b = 'c';
+// 	double c = 3.2f;
+
+// 	print(a);
+// 	print(b);
+// 	print(c);
+
+// 	return 0;
+// }
+
+/* Date 클래스 */
+// #include <iostream>
+
+// class Date
+// {
+// 	int year_;
+// 	int month_; // 1 부터 12 까지.
+// 	int day_;	// 1 부터 31 까지.
+
+// public:
+// 	void SetDate(int year, int month, int date);
+// 	void AddDay(int inc);
+// 	void AddMonth(int inc);
+// 	void AddYear(int inc);
+
+// 	// 해당 월의 총 일 수를 구한다.
+// 	int GetCurrentMonthTotalDays(int year, int month);
+
+// 	void ShowDate();
+// };
+
+// void Date::SetDate(int year, int month, int day)
+// {
+// 	year_ = year;
+// 	month_ = month;
+// 	day_ = day;
+// }
+
+// int Date::GetCurrentMonthTotalDays(int year, int month)
+// {
+// 	static int month_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+// 	if (month != 2)
+// 		return (month_day[month - 1]);
+// 	else if (year % 4 == 0 && year % 100 != 0)
+// 		return (29);
+// 	else
+// 		return (28);
+// }
+
+// void Date::AddDay(int inc)
+// {
+// 	while (true)
+// 	{
+// 		int current_month_total_days = GetCurrentMonthTotalDays(year_, month_);
+
+// 		if (day_ + inc <= current_month_total_days)
+// 		{
+// 			day_ += inc;
+// 			return;
+// 		}
+// 		else
+// 		{
+// 			inc -= (current_month_total_days - day_ + 1);
+// 			day_ = 1;
+// 			AddMonth(1);
+// 		}
+// 	}
+// }
+
+// void Date::AddMonth(int inc) {
+// 	AddYear((inc + month_ - 1) / 12);
+// 	month_ = month_ + inc % 12;
+// 	month_ = (month_ == 12 ? 12 : month_ % 12);
+// }
+
+// void Date::AddYear(int inc) { year_ += inc; }
+
+// void Date::ShowDate() {
+// 	std::cout << "오늘은" << year_ << " 년" << month_ << " 월" << day_ << " 일 입니다" << std::endl;
+// }
+
+// int main() {
+// 	Date day;
+// 	day.SetDate(2011, 3, 1);
+// 	day.ShowDate();
+
+// 	day.AddDay(30);
+// 	day.ShowDate();
+
+// 	day.AddDay(2000);
+// 	day.ShowDate();
+
+// 	day.SetDate(2012, 1, 31);
+// 	day.AddDay(29);
+// 	day.ShowDate();
+
+// 	day.SetDate(2012, 8 ,4);
+// 	day.AddDay(2500);
+// 	day.ShowDate();
+// 	return 0;
+// }
+
+/* 생성자 */
+// #include <iostream>
+
+// class Date
+// {
+// 	int year_;
+// 	int month_; // 1 부터 12 까지.
+// 	int day_;	// 1 부터 31 까지.
+
+// public:
+// 	void SetDate(int year, int month, int date);
+// 	void AddDay(int inc);
+// 	void AddMonth(int inc);
+// 	void AddYear(int inc);
+
+// 	// 해당 월의 총 일수를 구한다.
+// 	int GetCurrentMonthTotalDays(int year, int month);
+
+// 	void ShowDate();
+
+// 	Date(int year, int month, int day)
+// 	{
+// 		year_ = year;
+// 		month_ = month;
+// 		day_ = day;
+// 	}
+// };
+
+// void Date::AddYear(int inc) { year_ += inc; }
+// void Date::ShowDate()
+// {
+// 	std::cout << "오늘은 " << year_ << " 년 " << month_ << " 월 " << day_ << " 일 입니다 " << std::endl;
+// }
+// int main()
+// {
+// 	Date day(2011, 3, 1);
+// 	day.ShowDate();
+// 	day.AddYear(10);
+// 	day.ShowDate();
+// 	return 0;
+// }
+
+// 디폴트 생성자 정의해보기
+#include <iostream>
+class Date {
+int year_;
+int month_; // 1 부터 12 까지.
+int day_; // 1 부터 31 까지.
+public:
+void ShowDate();
+Date() {
+year_ = 2012;
+month_ = 7;
+day_ = 12;
+}
+};
+void Date::ShowDate() {
+std::cout << "오늘은 " << year_ << " 년 " << month_ << " 월 " << day_
+<< " 일 입니다 " << std::endl;
+}
+int main() {
+Date day = Date();
+Date day2;
+day.ShowDate();
+day2.ShowDate();
+return 0;
+}
