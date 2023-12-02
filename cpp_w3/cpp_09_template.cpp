7370b15e25ce25b9af2fd579f4168cac775483b9
way-daeho <lavr2755@gmail.com>
2023-12-02 17:32:07 +0900

    Study chapter 9 - 1 template remain 9 - 2, 3, 4

--- cpp_w3/cpp_09_template.cpp
+++ cpp_w3/cpp_09_template.cpp
@@ -6,7 +6,7 @@
 /*   By: daehlee <daehlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 23:15:05 by daehlee           #+#    #+#             */
-/*   Updated: 2023/12/02 02:30:23 by daehlee          ###   ########.fr       */
+/*   Updated: 2023/12/02 17:28:47 by daehlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -418,18 +418,291 @@
 	함수 객체(Function Object - Functor) 의 도입
 */
 
-template <typename Cont, typename Comp>
+// template <typename Cont, typename Comp>
 
-void bubble_sort(Cont &cont, Comp &comp)
-{
-	for (int i = 0; i < count.size(); i++)
-	{
-		for (int j = i + 1; j < count.size(); j++)
-		{
-			if (!comp(cont[i], cont[j]))
-			{
-				cont.swap(i, j);
-			}
-		}
-	}
-}
+// void bubble_sort(Cont &cont, Comp &comp)
+// {
+// 	for (int i = 0; i < count.size(); i++)
+// 	{
+// 		for (int j = i + 1; j < count.size(); j++)
+// 		{
+// 			if (!comp(cont[i], cont[j]))
+// 			{
+// 				cont.swap(i, j);
+// 			}
+// 		}
+// 	}
+// }
+
+// #include <iostream>
+
+// template <typename T>
+// class Vector
+// {
+// 	T *data;
+// 	int capacity;
+// 	int length;
+
+// public:
+// 	// 어떤 타입을 보관하는지
+// 	typedef T value_type;
+
+// 	// 생성자
+// 	Vector(int n = 1) : data(new T[n]), capacity(n), length(0) {}
+
+// 	// 맨 뒤에 새로운 원소를 추가한다.
+// 	void push_back(int s)
+// 	{
+// 		if (capacity <= length)
+// 		{
+// 			int *temp = new T[capacity * 2];
+// 			for (int i = 0; i < length; i++)
+// 			{
+// 				temp[i] = data[i];
+// 			}
+// 			delete[] data;
+// 			data = temp;
+// 			capacity *= 2;
+// 		}
+
+// 		data[length] = s;
+// 		length++;
+// 	}
+
+// 	// 임의의 위치의 원소에 접근한다.
+// 	T operator[](int i) { return data[i]; }
+
+// 	// x 번째 위치한 원소를 제거한다.
+// 	void remove(int x)
+// 	{
+// 		for (int i = x + 1; i < length; i++)
+// 		{
+// 			data[i - 1] = data[i];
+// 		}
+// 		length--;
+// 	}
+
+// 	// 현재 벡터의 크기를 구한다.
+// 	int size() { return length; }
+
+// 	// i 번째 원소와 j 번째 원소 위치를 바꾼다.
+// 	void swap(int i, int j)
+// 	{
+// 		T temp = data[i];
+// 		data[i] = data[j];
+// 		data[j] = temp;
+// 	}
+
+// 	~Vector()
+// 	{
+// 		if (data)
+// 		{
+// 			delete[] data;
+// 		}
+// 	}
+// };
+
+// template <typename Cont>
+// void bubble_sort(Cont &cont)
+// {
+// 	for (int i = 0; i < cont.size(); i++)
+// 	{
+// 		for (int j = i + 1; j < cont.size(); j++)
+// 		{
+// 			if (cont[i] > cont[j])
+// 			{
+// 				cont.swap(i, j);
+// 			}
+// 		}
+// 	}
+// }
+
+// template <typename Cont, typename Comp>
+// void bubble_sort(Cont &cont, Comp &comp)
+// {
+// 	for (int i = 0; i < cont.size(); i++)
+// 	{
+// 		for (int j = i + 1; j < cont.size(); j++)
+// 		{
+// 			if (!comp(cont[i], cont[j]))
+// 			{
+// 				cont.swap(i, j);
+// 			}
+// 		}
+// 	}
+// }
+
+// struct Comp1
+// {
+// 	bool operator()(int a, int b) { return a > b; }
+// };
+
+// struct Comp2
+// {
+// 	bool operator()(int a, int b) { return a < b; }
+// };
+
+// int main()
+// {
+// 	Vector<int> int_vec;
+// 	int_vec.push_back(3);
+// 	int_vec.push_back(1);
+// 	int_vec.push_back(2);
+// 	int_vec.push_back(8);
+// 	int_vec.push_back(5);
+// 	int_vec.push_back(3);
+
+// 	std::cout << "정렬 이전 ---- " << std::endl;
+// 	for (int i = 0; i < int_vec.size(); i++)
+// 	{
+// 		std::cout << int_vec[i] << " ";
+// 	}
+
+// 	Comp1 comp1;
+// 	bubble_sort(int_vec, comp1);
+
+// 	std::cout << std::endl
+// 			  << std::endl
+// 			  << "내림차순 정렬 이후 ---- " << std::endl;
+// 	for (int i = 0; i < int_vec.size(); i++)
+// 	{
+// 		std::cout << int_vec[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+
+// 	Comp2 comp2;
+// 	bubble_sort(int_vec, comp2);
+
+// 	std::cout << std::endl
+// 			  << "오름차순 정렬 이후 ---- " << std::endl;
+// 	for (int i = 0; i < int_vec.size(); i++)
+// 	{
+// 		std::cout << int_vec[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// }
+
+/* 타입이 아닌 템플릿 인자 (non - type template arguments) */
+// #include <iostream>
+
+// template <typename T, int num>
+// T add_num(T t)
+// {
+// 	return t + num;
+// }
+
+// int main()
+// {
+// 	int x = 3;
+// 	std::cout << "x : " << add_num<int, 5>(x) << std::endl;
+// }
+
+// #include <iostream>
+// #include <array>
+
+// int main()
+// {
+// 	// 마치 C 에서의 배열처럼 {} 을 통해 배열을 정의할 수 있다.
+// 	// {} 문법은 16 - 1 강에서 자세히 다루므로 여기서는 그냥
+// 	// 쓰면 되는 구나 이해하고 넘어가면 된다.
+// 	std::array<int, 5> arr = {1, 2, 3, 4, 5};
+// 	// int arr[5] = {1, 2, 3, 4, 5}; 와 동일
+
+// 	for (int i = 0; i < arr.size(); i++)
+// 	{
+// 		std::cout << arr[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// }
+
+// #include <iostream>
+// #include <array>
+
+// void print_array(const std::array<int, 5> &arr)
+// {
+// 	for (int i = 0; i < arr.size(); i++)
+// 	{
+// 		std::cout << arr[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// }
+
+// int main()
+// {
+// 	std::array<int, 5> arr = {1, 2, 3, 4, 5};
+
+// 	print_array(arr);
+// }
+
+// #include <iostream>
+// #include <array>
+
+// template <typename T>
+// void print_array(const T &arr)
+// {
+// 	for (int i = 0; i < arr.size(); i++)
+// 	{
+// 		std::cout << arr[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// }
+
+// int main()
+// {
+// 	std::array<int, 5> arr = {1, 2, 3, 4, 5};
+// 	std::array<int, 7> arr2 = {1, 2, 3, 4, 5, 6, 7};
+// 	std::array<int, 3> arr3 = {1, 2, 3};
+
+// 	print_array(arr);
+// 	print_array(arr2);
+// 	print_array(arr3);
+// }
+
+/*
+	디폴트 템플릿 인자
+	타입 역시 디폴트로 지정이 가능.
+*/
+
+// #include <iostream>
+
+// template <typename T, int num = 5>
+// T add_num(T t)
+// {
+// 	return t + num;
+// }
+
+// int main()
+// {
+// 	int x = 3;
+// 	std::cout << "x : " << add_num(x) << std::endl;
+// }
+
+// #include <iostream>
+// #include <string>
+
+// template <typename T>
+// struct Compare
+// {
+// 	bool operator()(const T &a, const T &b) const { return a < b; }
+// };
+
+// template <typename T, typename Comp = Compare<T>>
+// T Min(T a, T b)
+// {
+// 	Comp comp;
+// 	if (comp(a, b))
+// 	{
+// 		return a;
+// 	}
+// 	return b;
+// }
+
+// int main()
+// {
+// 	int a = 3, b = 5;
+// 	std::cout << "Min " << a << " , " << b << " :: " << Min(a, b) << std::endl;
+
+// 	std::string s1 = "abc", s2 = "def";
+// 	std::cout << "Min " << s1 << " , " << s2 << " :: " << Min(s1, s2)
+// 			  << std::endl;
+// }
