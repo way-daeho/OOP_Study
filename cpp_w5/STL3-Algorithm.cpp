eaef4e98477a40e5c77ff7b049f94ec44e57aa8b
way-daeho <lavr2755@gmail.com>
2023-12-15 13:34:38 +0900

    Finished 10 - 3

--- cpp_w5/STL3-Algorithm.cpp
+++ cpp_w5/STL3-Algorithm.cpp
@@ -513,7 +513,78 @@
 // }
 
 /* 원소를 탐색하는 함수 */
-#include <algorithm>
+// #include <algorithm>
+// #include <functional>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// template <typename Iter>
+// void print(Iter begin, Iter end)
+// {
+// 	while (begin != end)
+// 	{
+// 		std::cout << "[" << *begin << "] ";
+// 		begin++;
+// 	}
+// 	std::cout << std::endl;
+// }
+
+// int main()
+// {
+// 	std::vector<int> vec;
+// 	vec.push_back(5);
+// 	vec.push_back(3);
+// 	vec.push_back(1);
+// 	vec.push_back(2);
+// 	vec.push_back(3);
+// 	vec.push_back(4);
+
+// 	auto result = std::find(vec.begin(), vec.end(), 3);
+// 	std::cout << "3 은 " << std::distance(vec.begin(), result) + 1 << " 번째 원소"
+// 			  << std::endl;
+// }
+
+/* 중복되는 모든 값을 찾고 싶다! */
+// #include <algorithm>
+// #include <functional>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// template <typename Iter>
+// void print(Iter begin, Iter end)
+// {
+// 	while (begin != end)
+// 	{
+// 		std::cout << "[" << *begin << "] ";
+// 		begin++;
+// 	}
+// 	std::cout << std::endl;
+// }
+
+// int main()
+// {
+// 	std::vector<int> vec;
+// 	vec.push_back(5);
+// 	vec.push_back(3);
+// 	vec.push_back(1);
+// 	vec.push_back(2);
+// 	vec.push_back(3);
+// 	vec.push_back(4);
+
+// 	auto current = vec.begin();
+// 	while (true)
+// 	{
+// 		current = std::find(current, vec.end(), 3);
+// 		if (current == vec.end())
+// 			break;
+// 		std::cout << "3 은 " << std::distance(vec.begin(), current) + 1 << " 번째 원소" << std::endl;
+// 		current++;
+// 	}
+// }
+
+/* #include <algorithm>
 #include <functional>
 #include <iostream>
 #include <string>
@@ -540,7 +611,93 @@ int main()
 	vec.push_back(3);
 	vec.push_back(4);
 
-	auto result = std::find(vec.begin(), vec.end(), 3);
-	std::cout << "3 은 " << std::distance(vec.begin(), result) + 1 << " 번째 원소"
-			  << std::endl;
-}
+	auto current = vec.begin();
+	while (true)
+	{
+		current =
+			std::find_if(current, vec.end(), [](int i)
+						 { return i % 3 == 2; });
+		if (current == vec.end())
+			break;
+		std::cout << "3 으로 나눈 나머지가 2 인 원소는 : " << *current << " 이다 "
+				  << std::endl;
+		current++;
+	}
+} */
+/* any_of, all_of*/
+// #include <algorithm>
+// #include <functional>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// template <typename Iter>
+// void print(Iter begin, Iter end)
+// {
+// 	while (begin != end)
+// 	{
+// 		std::cout << "[" << *begin << "] ";
+// 		begin++;
+// 	}
+// 	std::cout << std::endl;
+// }
+
+// struct User
+// {
+// 	std::string name;
+// 	int level;
+
+// 	User(std::string name, int level) : name(name), level(level) {}
+// 	bool operator==(const User &user) const
+// 	{
+// 		if (name == user.name && level == user.level)
+// 			return true;
+// 		return false;
+// 	}
+// };
+
+// class Party
+// {
+// 	std::vector<User> users;
+
+// public:
+// 	bool add_user(std::string name, int level)
+// 	{
+// 		User new_user(name, level);
+// 		if (std::find(users.begin(), users.end(), new_user) != users.end())
+// 		{
+// 			return false;
+// 		}
+// 		users.push_back(new_user);
+// 		return true;
+// 	}
+
+// 	// 파티원 모두가 15 레벨 이상이여야지 던전 입장 가능
+// 	bool can_join_dungeon()
+// 	{
+// 		return std::all_of(users.begin(), users.end(),
+// 						   [](User &user)
+// 						   { return user.level >= 15; });
+// 	}
+
+// 	// 파티원 중 한명 이라도 19렙 이상이면 특별 아이템 사용 가능
+// 	bool can_use_special_item()
+// 	{
+// 		return std::any_of(users.begin(), users.end(),
+// 						   [](User &user)
+// 						   { return user.level >= 19; });
+// 	}
+// };
+// int main()
+// {
+// 	Party party;
+// 	party.add_user("철수", 15);
+// 	party.add_user("영희", 18);
+// 	party.add_user("민수", 12);
+// 	party.add_user("수빈", 19);
+
+// 	std::cout << std::boolalpha;
+// 	std::cout << "던전 입장 가능 ? " << party.can_join_dungeon() << std::endl;
+// 	std::cout << "특별 아이템 사용 가능 ? " << party.can_use_special_item()
+// 			  << std::endl;
+// }
