59a12727afcd26bfe91be248c0e6b2e3d0132662
way-daeho <lavr2755@gmail.com>
2024-01-09 02:10:14 +0900

    Finished Section2 and begining section 3 part operator

---
+++ JAVA/first-java/src/Variable/ex/VarEx1Question.java
@@ -0,0 +1,12 @@
+package Variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+
+        System.out.println(num1 - num2);
+        System.out.println(num1 + num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ JAVA/first-java/src/Variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package Variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+    }
+}
